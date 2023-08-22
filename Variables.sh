2e30c892ec5d05f46b1ced07c3a03d22bdb6c208
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-22 10:46:49 +0530

    Script Notes

---
+++ Variables.sh
@@ -0,0 +1,32 @@
+#sed -i -ie '5d' path
+
+#sed -i -e '/nlogoin/d' path
+
+##sed -i -e 's|12.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+##echo hello world
+
+#echo is to print
+
+#echo -e "\e[COLmMessage\e[0m"
+
+#here COL is color of the line colors will be of
+ ## 31m = red 32m = Green 33m = yellow 34m = blue 35m = magenta 36m = cyan
+
+
+ echo -e "\e[32m>>>>>>> Inatsll NodeJS >>>>>>>\e[0m"
+
+ var = Data
+ in bash we call it as $VAR
+
+ #Dynamic varaiables are VAR = $(COMMAND) ---- Command variable
+ VAR = $((EXPRESSION))-------- Arthiematic variable
+
+ ex: echo name = ${name}
+
+ Date = $(date +%F)
+ echo today date is $Date
+
+ Arth =$((2+3*4/2))
+
+ echo arth =${Arth}
