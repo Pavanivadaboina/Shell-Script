d15ab1c5ea6ecba4948d3cb27a9777baf53dc14d
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-23 10:09:13 +0530

    shipping and RabbitMQ headings

---
+++ 02 variable.sh
@@ -0,0 +1,13 @@
+ #Shell script will run the command inside $() and stores the output into the vatriable Date
+
+ Date=$(date)
+ echo "this script is executed at time: $Date"
+
+
+ Name1:$1
+ Name2:$2
+ Name3:$3
+
+echo "Name1 is a good boy"
+echo "Name2 and Name1 are best friends"
+echo "Name3 and name2 are intelligent"
--- Practicevar.sh
+++
@@ -1,4 +0,0 @@
- #Shell script will run the command inside $() and stores the output into the vatriable Date
-
- Date=$(date)
- echo "this script is executed at time: $Date"
