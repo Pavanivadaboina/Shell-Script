2cf3a2890fb84ff62597aac000bfd112f898405a
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-23 11:14:18 +0530

    shell script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,12 +1,12 @@
- #Shell script will run the command inside $() and stores the output into the vatriable Date
+ #Shell script will run the command inside $() and stores the output into the variable Date
 
  Date=$(date)
  echo "this script is executed at time: $Date"
 
 
- Name1=$1
- Name2=$2
- Name3=$3
+ Name1=$1 # this is first value from command line
+ Name2=$2 # this is second value from command line
+ Name3=$3 # this is third value from command line
 
 echo "$Name1: $Name1 is a good boy"
 echo "$Name2: $Name2 friends"
