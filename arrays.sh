40da709e5eab03de046ac6c9a525188c1218a0ea
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-23 12:30:27 +0530

    shell script practice

---
+++ arrays.sh
@@ -0,0 +1,15 @@
+
+
+#array can hold multiple values
+
+Players=("tina" "simth" "ritu")
+
+# here players is a variable and names are values
+
+#inside array we always refer values with index and it starts with 0####
+
+echo "frist player: $player[0]"
+echo "all players: $players[@]"
+
+###here @ means everythinhg inside the array##
+##the values [0] [1] are index values
