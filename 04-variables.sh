57ac7911752ceb19c51dcc7af437cddcb2913142
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-23 11:59:12 +0530

    shell script practice

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,10 +4,12 @@ echo please enter the username
 
 read -s username
 
+#this is for practice purpose we shouldn't print the username/password
 echo "username entered is: $username"
 
 echo please enter the password
 
 read -s password
 
+#this is for practice purpose we shouldn't print the username/password
 echo "password entered is:  $password"
