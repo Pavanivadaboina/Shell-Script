7cb870f1223eb34340d30669bf0ed86a0e048754
pavanivadaboina <Pavanivadaboina567@gmail.com>
2023-08-25 00:21:34 +0530

    cart and common.sh code dry

--- conditions.sh
+++ conditions.sh
@@ -3,3 +3,16 @@ special veriables
 # -z is to check whether the variable is empty or not
 
 
+noddles_veg=$1
+price=$2
+
+if [ -z "noddles_veg"]; then
+  echo show no item avaiable
+  exit
+fi
+
+  if [ "noddles_veg" == "free"];
+  then
+    echo free pirce - $price
+    else
+      echo no noddles availble
